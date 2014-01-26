3fe39b37b40a51d49ef4d1b157a1df50d0e6b343
Andi Sidwell <andi@takkaria.org>
2014-01-26 00:54:33 +0000

    Alter algorithm again and document

--- js/bonds.js
+++ js/bonds.js
@@ -20,18 +20,25 @@ function init() {
 };
 
 // Task: Test sums of the form (10+x) - y, where y > x.
+// y is between 3-9 since y<3 is not interesting (you can get 11-2, 10-2 or 10-1)
+// x is actually < (y-1) because when 9s are easy to work out (12-3 ... 18-9)
 function newFlashcard() {
 
 /*	var x = Math.random1(8);
 	var y = x + 1 + Math.random1(8 - x); */
 
-	// Generate y first, between 2 and 9
-	var y = 1 + Math.random1(8);
+	// Generate y first, between 3 and 9
+	var y = 2 + Math.random1(7);
 	// Generate x, smaller than y
-	var x = Math.random0(y - 1);
+	var x = Math.random0(y - 2);
 
 	x += 10;
 	var text = x + " - " + y;
+
+	if (state.answer == x - y) {
+		newFlashcard();
+		return;
+	}
 
 	state.answer = x - y;
 
